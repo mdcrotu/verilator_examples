407e0ac63cfe7d80c9fff87854f423435e446dde
Mike Cochran <mdcrotu@gmail.com>
2025-05-12 11:45:40 -0400

    Working on this file...

--- simple_dpi/multiplier/multiplier.sv
+++ simple_dpi/multiplier/multiplier.sv
@@ -1,24 +1,36 @@
 // SystemVerilog module
-module multiplier (input logic clk, input logic rst, input logic signed [31:0] a, input logic signed [31:0] b, output logic signed [31:0] result);
+module multiplier (input logic signed [31:0] a, input logic signed [31:0] b, output logic signed [31:0] result);
 
   import "DPI-C" function int multiply(input int a, input int b);
 
-//  always_ff @(posedge clk) begin
-//    if (rst) begin
-//      result <= 0;
-//    end else begin
-//      result <= multiply(a, b);
-//    end
-//  end
-
-  reg [31:0] x, y, product;
-
-  initial begin
-    x = 10;
-    y = 20;
-    product = multiply(x, y);
-    $display("%d * %d = %d", x, y, product);
-    $finish;  
+  always_ff @(posedge clk) begin
+    if (rst) begin
+      result <= 0;
+    end else begin
+      result <= multiply(a, b);
+    end
   end
+
+  parameter int clk_tgl_period = 5;
+  parameter timeout = 500;    
+  reg clk;
+  reg rst;
+  initial begin 
+            clk = 0;
+            rst = 1;
+            #(timeout) $finish;
+  end
+  always #(clk_tgl_period) clk = ~clk;
+
+        
+//  reg [31:0] x, y, product;
+
+//  initial begin
+//    x = 10;
+//    y = 20;
+//    product = multiply(x, y);
+//    $display("%d * %d = %d", x, y, product);
+//    $finish;  
+//  end
   
 endmodule
